98e4d7afaadaf2ee7d6dbffe7650863a04116ff7
James Zern <jzern@google.com>
2024-12-19 18:45:59 -0800

    set_analyzer_env.sh: add float-cast-overflow w/ubsan
    Tested with clang-14.
    
    Change-Id: I396a091b7a9910fdd9cd3db7ac2e848b49d78ca5

--- tools/set_analyzer_env.sh
+++ tools/set_analyzer_env.sh
@@ -73,12 +73,17 @@ cflags="${cflags} -fno-omit-frame-pointer"
 # Exact backtraces.
 cflags="${cflags} -fno-optimize-sibling-calls"
 
-if [ "${sanitizer}" = "cfi" ]; then
-  # https://clang.llvm.org/docs/ControlFlowIntegrity.html
-  cflags="${cflags} -fno-sanitize-trap=cfi -flto -fvisibility=hidden"
-  ldflags="${ldflags} -fno-sanitize-trap=cfi -flto -fuse-ld=gold"
-  export AR="llvm-ar"
-fi
+case "${sanitizer}" in
+  cfi)
+    # https://clang.llvm.org/docs/ControlFlowIntegrity.html
+    cflags="${cflags} -fno-sanitize-trap=cfi -flto -fvisibility=hidden"
+    ldflags="${ldflags} -fno-sanitize-trap=cfi -flto -fuse-ld=gold"
+    export AR="llvm-ar"
+    ;;
+  integer|undefined)
+    cflags="${cflags} -fsanitize=float-cast-overflow"
+    ;;
+esac
 
 set -x
 export CC="clang"
