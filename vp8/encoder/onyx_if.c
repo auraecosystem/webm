0ca5ac236d789111891e3a84bb4012469d844a57
Philipp Hancke <phancke@meta.com>
2025-01-09 15:25:11 -0800

    Allow per-frame calculation of PSNR
    See here for the libaom change:
      https://aomedia-review.googlesource.com/c/aom/+/196501
    and here for the WebRTC change with the intended usage:
      https://webrtc-review.googlesource.com/c/src/+/368960
    
    BUG=webrtc:388070060
    
    Change-Id: Id2d13d94a63a802991bfcb7908540dad91ea29fc

--- vp8/encoder/onyx_if.c
+++ vp8/encoder/onyx_if.c
@@ -5144,7 +5144,8 @@ int vp8_get_compressed_data(VP8_COMP *cpi, unsigned int *frame_flags,
   }
 #endif
 
-  if (cpi->b_calculate_psnr && cpi->pass != 1 && cm->show_frame) {
+  if ((cpi->b_calculate_psnr || (*frame_flags & FRAMEFLAGS_PSNR)) &&
+      cpi->pass != 1 && cm->show_frame) {
     generate_psnr_packet(cpi);
   }
 
@@ -5157,7 +5158,7 @@ int vp8_get_compressed_data(VP8_COMP *cpi, unsigned int *frame_flags,
       cpi->common.show_frame_mi = cpi->common.mi;
       cpi->count++;
 
-      if (cpi->b_calculate_psnr) {
+      if (cpi->b_calculate_psnr || (*frame_flags & FRAMEFLAGS_PSNR)) {
         uint64_t ye, ue, ve;
         double frame_psnr;
         YV12_BUFFER_CONFIG *orig = cpi->Source;
--- vpx/src/vpx_encoder.c
+++ vpx/src/vpx_encoder.c
@@ -205,7 +205,12 @@ vpx_codec_err_t vpx_codec_encode(vpx_codec_ctx_t *ctx, const vpx_image_t *img,
   else if (duration > UINT32_MAX || deadline > UINT32_MAX)
     res = VPX_CODEC_INVALID_PARAM;
 #endif
-  else {
+  else if ((flags & VPX_EFLAG_CALCULATE_PSNR) &&
+           (!(ctx->iface->caps & VPX_CODEC_CAP_PSNR) ||
+            ctx->config.enc->g_lag_in_frames != 0)) {
+    ctx->err_detail = "Conflicting PSNR flags.";
+    res = VPX_CODEC_INCAPABLE;
+  } else {
     unsigned int num_enc = ctx->priv->enc.total_encoders;
 
     /* Execute in a normalized floating point environment, if the platform
