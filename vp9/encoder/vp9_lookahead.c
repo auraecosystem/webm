311c561d3e8c9ba160383e86c4c508523b19eb86
Marco Paniconi <marpan@google.com>
2025-03-13 19:34:18 -0700

    vp9: fix to tsan race for external resize
    Set/update the stride values on a resized frame.
    
    Added new external resize tests, for single layer
    and svc case, that trigger the same issue. Tests
    run with 4 threads and small keyframe spacing. Issue
    was occuring on keyframes after resolution went down
    from 640x480 to 320x240.
    
    This issue was originally seen in a webrtc svc test
    with the patch:
    https://webrtc-review.googlesource.com/c/src/+/374661
    
    Change-Id: I45211b773af808a317de0a4a1259ca17d695b8c6

--- vp9/encoder/vp9_lookahead.c
+++ vp9/encoder/vp9_lookahead.c
@@ -95,6 +95,7 @@ int vp9_lookahead_push(struct lookahead_ctx *ctx, YV12_BUFFER_CONFIG *src,
                        vpx_enc_frame_flags_t flags) {
   struct lookahead_entry *buf;
   int width = src->y_crop_width;
+  int aligned_width = ALIGN_POWER_OF_TWO(width, 3);
   int height = src->y_crop_height;
   int uv_width = src->uv_crop_width;
   int uv_height = src->uv_crop_height;
@@ -142,6 +143,9 @@ int vp9_lookahead_push(struct lookahead_ctx *ctx, YV12_BUFFER_CONFIG *src,
     buf->img.uv_crop_height = src->uv_crop_height;
     buf->img.subsampling_x = src->subsampling_x;
     buf->img.subsampling_y = src->subsampling_y;
+    buf->img.y_stride =
+        ALIGN_POWER_OF_TWO((aligned_width + 2 * buf->img.border), 5);
+    buf->img.uv_stride = buf->img.y_stride >> subsampling_x;
   }
   vp9_copy_and_extend_frame(src, &buf->img);
 
