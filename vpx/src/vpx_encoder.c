8e7a3cee1e1bac40cd7037cfcf725dc80530d429
Marco Paniconi <marpan@google.com>
2025-04-29 15:33:58 -0700

    Possible fix to multi-res encoding issue
    Replace mem_loc_owned to mr_cfg, to properly track
    when the mem_loc is freed.
    
    Bug: b:413411335
    Change-Id: I64f9afdbbeb1af9e80652901a7c66fb041082ef5

--- vpx/src/vpx_encoder.c
+++ vpx/src/vpx_encoder.c
@@ -87,9 +87,6 @@ vpx_codec_err_t vpx_codec_enc_init_multi_ver(
     res = VPX_CODEC_INCAPABLE;
   else {
     int i;
-#if CONFIG_MULTI_RES_ENCODING
-    int mem_loc_owned = 0;
-#endif
     void *mem_loc = NULL;
 
     if (iface->enc.mr_get_mem_loc == NULL) return VPX_CODEC_INCAPABLE;
@@ -97,6 +94,7 @@ vpx_codec_err_t vpx_codec_enc_init_multi_ver(
     if (!(res = iface->enc.mr_get_mem_loc(cfg, &mem_loc))) {
       for (i = 0; i < num_enc; i++) {
         vpx_codec_priv_enc_mr_cfg_t mr_cfg;
+        if (i == 0) mr_cfg.mem_loc_owned = 0;
 
         /* Validate down-sampling factor. */
         if (dsf->num < 1 || dsf->num > 4096 || dsf->den < 1 ||
@@ -131,7 +129,7 @@ vpx_codec_err_t vpx_codec_enc_init_multi_ver(
             i--;
           }
 #if CONFIG_MULTI_RES_ENCODING
-          if (!mem_loc_owned) {
+          if (!mr_cfg.mem_loc_owned) {
             assert(mem_loc);
             free(((LOWER_RES_FRAME_INFO *)mem_loc)->mb_info);
             free(mem_loc);
@@ -139,9 +137,6 @@ vpx_codec_err_t vpx_codec_enc_init_multi_ver(
 #endif
           return SAVE_STATUS(ctx, res);
         }
-#if CONFIG_MULTI_RES_ENCODING
-        mem_loc_owned = 1;
-#endif
         ctx++;
         cfg++;
         dsf++;
