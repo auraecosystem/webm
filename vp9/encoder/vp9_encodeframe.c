9915255006356a210f13ee83d5015dff35b96fdb
Marco Paniconi <marpan@google.com>
2025-08-11 23:11:20 -0700

    vp9: Fix and test case for partitioning assert issue
    Triggered for 1 pass, good_quality, lossless,
    speed 0 or 1.
    
    See comment #22 in issue below.
    
    Fix is to avoid the pruning decision in
    ml_predict_var_rd_partitioning() to keep the
    partition_none_allowed set to 1, for bsize = BLOCK_8X8
    on the boundary. Otherwise this can cause partition to
    extend across boundary and assert is triggered in
    vp9_bitstream.c (write_partition() function).
    
    Bug: b:433941753
    Change-Id: I359d64a6d95d3f8a5493bfa924a48bd402b97881

--- vp9/encoder/vp9_encodeframe.c
+++ vp9/encoder/vp9_encodeframe.c
@@ -3901,6 +3901,15 @@ static int rd_pick_partition(VP9_COMP *cpi, ThreadData *td,
     if (do_rd_ml_partition_var_pruning) {
       ml_predict_var_rd_partitioning(cpi, x, pc_tree, bsize, mi_row, mi_col,
                                      &partition_none_allowed, &do_split);
+      // On the boundary for bsize = BLOCK_8X8: keep partition_none_allowed = 1
+      // as ml_predict_var_rd_partitioning() may prune it out (set it to 0). If
+      // this happens and only split is skipped below then the 64x64 may be set
+      // which will extend outside the range.
+      if ((mi_row + num_8x8_blocks_high_lookup[bsize] == cm->mi_rows ||
+           mi_col + num_8x8_blocks_wide_lookup[bsize] == cm->mi_cols) &&
+          bsize == BLOCK_8X8) {
+        partition_none_allowed = 1;
+      }
     } else {
       vp9_zero(pc_tree->mv);
     }
