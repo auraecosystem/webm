29f4c7de3b97352bbfb11f7ef56fc37169c4a5a0
James Zern <jzern@google.com>
2025-08-29 15:21:28 -0700

    calc_pframe_target_size: remove some dead code
    The block is executed when `cpi->last_boost > 150`, `Adjustment` is
    calculated by `(cpi->last_boost - 100) >> 5` which at a minimum is 1.
    This change removes an `Adjustment < 1` check.
    
    Fixes Coverity issue:
    
    1568643 Logically dead code
    The indicated dead code may have performed some action; that action will
    never occur.
    
    In calc_pframe_target_size: Code can never be reached because of a
    logical contradiction (CWE-561)
    
    cond_at_least: Condition cpi->last_boost > 150, taking true branch. Now
    the value of cpi->last_boost is at least 151.
    641  if ((cpi->last_boost > 150) && (cpi->frames_till_gf_update_due > 0) &&
    642      (cpi->current_gf_interval >= (MIN_GF_INTERVAL << 1))) {
    643    /* % Adjustment limited to the range 1% to 10% */
        assignment: Assigning: Adjustment = cpi->last_boost - 100 >> 5.
    644    Adjustment = (cpi->last_boost - 100) >> 5;
    645
        at_least: At condition Adjustment < 1, the value of Adjustment
        must be at least 1.
        dead_error_condition: The condition Adjustment < 1 cannot be
        true.
    646    if (Adjustment < 1) {
    
    CID 1568643: (#1 of 1): Logically dead code (DEADCODE)
    dead_error_line: Execution cannot reach this statement: Adjustment = 1;.
    
    Change-Id: I9754c7f27678e29e4952f826380f8d4c6be805d1

--- vp8/encoder/ratectrl.c
+++ vp8/encoder/ratectrl.c
@@ -643,9 +643,7 @@ static void calc_pframe_target_size(VP8_COMP *cpi) {
         /* % Adjustment limited to the range 1% to 10% */
         Adjustment = (cpi->last_boost - 100) >> 5;
 
-        if (Adjustment < 1) {
-          Adjustment = 1;
-        } else if (Adjustment > 10) {
+        if (Adjustment > 10) {
           Adjustment = 10;
         }
 
