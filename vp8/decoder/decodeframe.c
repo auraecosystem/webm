cb8c7ad514c448d2e0d03a210a38b64cca27ed1a
James Zern <jzern@google.com>
2026-02-12 15:00:36 -0800

    vp8_decode_frame: improve partition 0 size check
    Include the remaining uncompressed data size in the check against
    `data_end`. The first partition size does not include the uncompressed
    data.
    
    Change-Id: Ibda173cc7777ae4602c7495f84814e200a21338f

--- vp8/decoder/decodeframe.c
+++ vp8/decoder/decodeframe.c
@@ -923,15 +923,16 @@ int vp8_decode_frame(VP8D_COMP *pbi) {
     pc->show_frame = (clear[0] >> 4) & 1;
     first_partition_length_in_bytes =
         (clear[0] | (clear[1] << 8) | (clear[2] << 16)) >> 5;
+    data += 3;
+    clear += 3;
 
-    if (!pbi->ec_active && (data + first_partition_length_in_bytes > data_end ||
+    const int min_data_length =
+        first_partition_length_in_bytes + (pc->frame_type == KEY_FRAME ? 7 : 0);
+    if (!pbi->ec_active && (data + min_data_length > data_end ||
                             first_partition_length_in_bytes == 0)) {
       vpx_internal_error(&pc->error, VPX_CODEC_CORRUPT_FRAME,
                          "Truncated packet or corrupt partition 0 length");
     }
-
-    data += 3;
-    clear += 3;
 
     vp8_setup_version(pc);
 
