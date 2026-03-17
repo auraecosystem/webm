27a161ec15a0c8b38a22c0663936926b64d2efdf
Marco Paniconi <marpan@google.com>
2026-03-17 11:40:45 -0700

    vp9: fix buffer overflow in vp9_get_token_cost
    Clamp the max_index for the vp9_cat6_high#_cost[] tables.
    
    Also clamp the see calculation in sum_squarss_visible(),
    as the test below causes overflow in that function.
    Similar to the change in encodeframe.c
    
    Enable the test that triggers the issue.
    
    Bug: 488585490
    
    Change-Id: I90800d9efe6c307310ef586bc5807af8088c2f41

--- vp9/encoder/vp9_encoder.h
+++ vp9/encoder/vp9_encoder.h
@@ -1292,6 +1292,11 @@ static int64_t sum_squares_visible(const MACROBLOCKD *xd,
       (b4x4s_to_right_edge >= tx_4x4_w && b4x4s_to_bottom_edge >= tx_4x4_h)) {
     assert(tx_4x4_w == tx_4x4_h);
     sse = (int64_t)vpx_sum_squares_2d_i16(diff, diff_stride, tx_4x4_w << 2);
+    // Bug: 488585490
+    // The test Buganizer488585490CostTableOverflow causes the computation
+    // for the sse in the above function to wrap around and go negative.
+    // Add this clamp to handle this.
+    if (sse < 0) sse = INT64_MAX;
     *visible_width = tx_4x4_w << 2;
     *visible_height = tx_4x4_h << 2;
   } else {
