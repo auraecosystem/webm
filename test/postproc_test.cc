284ab3216e63f01026ea42afd030ec070b86853b
James Zern <jzern@google.com>
2026-05-21 09:50:49 -0700

    vp9_postproc: fix buffer size mismatch on resize
    With `VP9D_MFQE` and either `VP9D_DEMACROBLOCK` or `VP9D_DEBLOCK`,
    `post_proc_buffer_int` is used, but was only allocated once. When the
    resolution changed this may have led to an out of bounds access.
    
    This change ensures `post_proc_buffer_int` and `ppbuf` are of the same
    size. There may be room to make the allocations smaller, but currently
    the dimensions are used interchangeably, so the larger aligned size is
    preferred.
    
    The generated noise buffer receives the same treatment.
    
    Bug: 499602810
    Change-Id: Iafd45a0d97c1b662ce38ba3c4c46d3f9095754c3

--- test/postproc_test.cc
+++ test/postproc_test.cc
@@ -193,14 +193,9 @@ VP8_INSTANTIATE_TEST_SUITE(PostProcTestInvalidFiles,
 #endif  // CONFIG_VP8_DECODER && CONFIG_POSTPROC
 
 #if CONFIG_VP9_DECODER && CONFIG_VP9_POSTPROC
-constexpr std::array<const char *, 12> kVP9FailingTestVectors = {
-  "vp90-2-14-resize-10frames-fp-tiles-1-2-4-8.webm",
-  "vp90-2-14-resize-10frames-fp-tiles-1-2.webm",
+constexpr std::array<const char *, 7> kVP9FailingTestVectors = {
   "vp90-2-14-resize-10frames-fp-tiles-1-4.webm",
   "vp90-2-14-resize-10frames-fp-tiles-1-8.webm",
-  "vp90-2-14-resize-10frames-fp-tiles-2-4.webm",
-  "vp90-2-14-resize-10frames-fp-tiles-2-8.webm",
-  "vp90-2-14-resize-10frames-fp-tiles-4-8.webm",
   "vp90-2-14-resize-10frames-fp-tiles-8-4-2-1.webm",
   "vp90-2-21-resize_inter_1920x1080_5_3-4.webm",
   "vp90-2-21-resize_inter_1920x1080_7_3-4.webm",
