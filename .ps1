d0855dd1be6cb4fb1c6cd17a0256f8e4c44140cb
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-26 02:33:16 +0100

    Add PowerShell script for Git cookie configuration

---
+++ .ps1
@@ -0,0 +1,2 @@
+git config --global http.cookiefile "%USERPROFILE%\.gitcookies"
+powershell -noprofile -nologo -command Write-Output ".googlesource.com`tTRUE`t/`tTRUE`t2147483647`to`tgit-kubulee.KL.gmail.com=1//03wF_f2UNFeCTCgYIARAAGAMSNwF-L9IrCZUaa1m0uJU49G3gopz7cEMCK8gtunOnlesWU9JyLgjUHzBB8cynSoC4PC5IqrdBBc4" >>"%USERPROFILE%\.gitcookies"
